031984598ca9231d2255ccdbb8fcffae278c55d0
Yvonne_Br <y.breitenbach@gmail.com>
2025-03-28 14:30:43 +0000

    corrected pep8 mistakes 2

--- file_test.py
+++ file_test.py
@@ -1,5 +1,3 @@
-#import pytest
-
 def test_calc_addition():
     # Function test the output of 2+4
     output = 2+4
@@ -7,18 +5,18 @@ def test_calc_addition():
 
 
 def test_calc_substraction():
-   # Function test the output of 2-4
+    # Function test the output of 2-4
     output = 2-4
     assert output == -2
 
 
 def test_calc_multiply():
-   # Function test the output of 2*4
+    # Function test the output of 2*4
     output = 2*4
     assert output == 8
 
 
 def test_coucou():
-   # Function test if the output return 'hello'
+    # Function test if the output return 'hello'
     output = 'hello'
     assert output == 'hello'
